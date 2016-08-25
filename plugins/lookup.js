dff308d33b6ad9272de40759a16564725e23f9a0
atf1999 <torth212@gmail.com>
2016-08-25 17:12:28 -0400

    decided to use a google search npm since google doesnt like being scraped

--- plugins/lookup.js
+++ plugins/lookup.js
@@ -1,11 +1,17 @@
-var googleSearch = require("../scrapers/google");
+var google = require('google-search-scraper');
 module.exports = {
     main: function(bot, message){
+       google.resultsPerPage = 25;
         var search = message.content;
         console.log(search);
-        bot.sendMessage(message, "googling", function(err, message){
-            googleSearch(search, function(res){
-                bot.updateMessage(message, res[0]);
+        var options = {
+          query: search,
+          limit: 1
+        };
+        bot.sendMessage(message, "Googling....", function(err, message){
+            google.search(options, function(err, res){
+                if(err) console.log(err);
+                bot.updateMessage(message, res);
             });
         });
     }
