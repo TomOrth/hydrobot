cc57eafbf206c95ee48e16b188db5e036a7e1cec
atf1999 <torth212@gmail.com>
2016-08-26 21:18:38 -0400

    went with memegen because meme-maker doesnt have use strict

--- plugins/meme.js
+++ plugins/meme.js
@@ -1,23 +1,9 @@
-var fse = require("fs-extra");
-var memeMaker = require("meme-maker");
 
 module.exports = {
     main: function(bot, message){
         var content = message.content,
             text = content.split("-");
-        var options = {
-            image: __dirname + "/templates/" + text[0] + ".jpg",
-            outfile: __dirname + "/" + message.author.name + "-" + message.server + ".jpg",
-            topText: text[1],
-            bottomText: text[2]
-        };
-        memeMaker(options, function(err) {
-            if(err) console.log(err);
-            bot.sendFile(message, options.outfile);
-            fse.remove(options.outfile, function(err){
-                if(err) console.log(err);
-            });
-        });
+        bot.sendMessage(message, "https://memegen.link/" + text[0] + "/" + text[1].split(" ").join("-") + "/" + text[2].split(" ").join("-") + ".jpg");
 
     }
 }
